a6208f6a901f2ea07c2b2b6b77dbb502a6d1ff1c
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-04-09 11:05:16 -0300

    feat(App): add skeleton to the site

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,123 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Máxima - Saúde Ocupacional</h1>
+      <nav>
+        <img src="" alt="Logo Máxima Padrão" />
+        <div>
+          <div>
+            <button>Início</button>
+            <button>Sobre</button>
+            <button>Serviços</button>
+            <button>Contato</button>
+          </div>
+          <button>
+            <h3>Atendimento</h3>
+            <p>+55 81 98294-2490</p>
+          </button>
+        </div>
+      </nav>
+      <section>
+        <article>
+          <h2>CENTRO DE MÉDICINA DO TRABALHO EM CARUARU-PE</h2>
+          <h1>
+            CUIDANDO DA SAÚDE OCUPACIONAL COM EXCELÊNCIA E RESPONSABILIDADE.
+          </h1>
+        </article>
+        <article>
+          <h2>Marque sua consulta agora!</h2>
+          <button>
+            <img src="" alt="Icone do WhatsApp" />
+            <p>WHATSAPP</p>
+          </button>
+        </article>
+        <button>
+          <img src="" alt="Seta para baixo" />
+        </button>
+      </section>
+      <section>
+        <h1>Bem-vindo à Máxima</h1>
+        <article>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            asperiores totam fugit, odit quisquam et provident quae nihil. Unde
+            perferendis, doloremque neque quibusdam officia a sequi. Sapiente
+            laborum aut sit?
+          </p>
+          <button>
+            <img src="" alt="Icone de Agenda" />
+            <p>AGENDAR CONSULTA</p>
+          </button>
+        </article>
+      </section>
+      <section>
+        <h1>Serviços Oferecidos</h1>
+        <article>
+          <h2>CONHEÇA OS PRINCIPAIS SERVIÇOS OFERECIDOS PELA MÁXIMA.</h2>
+          <div>
+            <div>
+              <img src="" alt="Imagem do Exame Admissional" />
+              <h3>EXAME ADMISSIONAL</h3>
+              <p>Realizado antes do início das atividades do trabalhador.</p>
+            </div>
+            <div>
+              <img src="" alt="Imagem do Exame Periódico" />
+              <h3>EXAME PERIÓDICO</h3>
+              <p>Feito regularmente, conforme o PCMSO de cada empresa.</p>
+            </div>
+            <div>
+              <img src="" alt="Imagem do Retorno do Trabalho" />
+              <h3>EXAME DE RETORNO DO TRABALHO</h3>
+              <p>Obrigatório após afastamento de 30 dias ou mais.</p>
+            </div>
+            <div>
+              <img src="" alt="Imagem do Exame de Mudança de Função" />
+              <h3>EXAME DE MUDANÇA DE FUNÇÃO</h3>
+              <p>Necessário se os riscos da nova função forem diferentes. </p>
+            </div>
+            <div>
+              <img src="" alt="Imagem do Exame Demissional" />
+              <h3>EXAME DEMISSIONAL</h3>
+              <p>Feito no desligamento para garantir condições de saúde.</p>
+            </div>
+          </div>
+        </article>
+      </section>
+      <section>
+        <article>
+          <h1>Faça seu agendamento</h1>
+          <p>
+            Para marcar uma consulta, tirar dúvidas ou solicitar informações,
+            você pode entrar em contato conosco pelo whatsapp:
+          </p>
+          <button>
+            <img src="" alt="Icone do WhatsApp" />
+            <p>WHATSAPP</p>
+          </button>
+          <p>
+            <span>Telefone:</span> +55 81 98294-2490
+          </p>
+          <h1>Nossa Localização</h1>
+          <p>Rua Nossa Senhora de Fátima, 158, Caruaru 55012600</p>
+          <a>
+            <p>Ver no Google Maps</p>
+            <img src="" alt="Seta" />
+          </a>
+        </article>
+        <article>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.241040715707!2d-35.972328625228435!3d-8.27879199175559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98bc303241c57%3A0x103c272412df0e90!2sM%C3%A1xima%20Medicina%20do%20Trabalho%20Caruaru%20PE!5e0!3m2!1spt-BR!2sbr!4v1775743132668!5m2!1spt-BR!2sbr"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </article>
+      </section>
+      <footer>
+        <img src="" alt="Logo Máxima Versão Branca" />
+        <p>Maxima © 2026 - Todos os direitos reservados</p>
+      </footer>
     </div>
   );
 }
